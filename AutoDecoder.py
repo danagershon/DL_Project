9b883d16bf2cc7a8268f5bf8e3cef9b8921218e8
danagershon <danagershon@h-iMac-sl-Dana.local>
2024-09-06 15:15:38 +0300

    updated docstrings, ensure feature_map_size param is used in the first conv layer. this commit a a stable implementation with BCE loss. reconstructed images look like original but more "pixilated"

--- AutoDecoder.py
+++ AutoDecoder.py
@@ -1,29 +1,32 @@
 import torch
 import torch.nn as nn
-
 import torch.optim
 import matplotlib.pyplot as plt
-
+import time
 import utils
 
 
 class AutoDecoder(nn.Module):
+    """
+    AutoDecoder class that maps latent vectors to reconstructed images using 
+    a fully connected layer and a decoder made of ConvTranspose2d layers.
+    """
+
     def __init__(self, latent_dim=64, img_channels=1, feature_map_size=512):
         """
-        Initialize the AutoDecoder.
         :param latent_dim: Dimensionality of the latent space
         :param img_channels: Number of image channels (1 for grayscale images in Fashion MNIST)
-        :param feature_map_size: 128/256
+        :param feature_map_size: Number of feature maps to first extract from the latent vector, before going throght CNN (e.g., 128/256/512)
         """
         super().__init__()
 
-        # Fully connected layers to expand the latent vector into a feature map
+        # Expand the latent vector into a feature map via a fully connected layer
         self.feature_map_size = feature_map_size
-        self.fc = nn.Linear(latent_dim, 7 * 7 * self.feature_map_size)
+        self.fc = nn.Linear(latent_dim, 7 * 7 * feature_map_size)
         
-        # Decoder architecture using ConvTranspose2d layers to reconstruct the image
+        # Decoder architecture. Using ConvTranspose2d layers to "reconstruct" the image
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(feature_map_size, 256, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.ReLU(),
             nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.ReLU(),
@@ -34,54 +37,96 @@ class AutoDecoder(nn.Module):
     def forward(self, z):
         """
         Forward pass of the auto-decoder.
-        :param z: the latent vector for each sample
+
+        :param z: the latent vector for the sample
         :return: the reconstructed image
         """
+        # Extract feature map from the latent vector
         z = self.fc(z)
-        z = z.view(-1, self.feature_map_size, 7, 7)  # Reshape to (batch_size, feature_map_size, height, width)
-        z = self.decoder(z)  # Apply the CNN decoder to reconstruct the image
-        return z
-    
+        # Reshape to (batch_size, feature_map_size, height, width)
+        z = z.view(-1, self.feature_map_size, 7, 7)
+        # Reconstruct the image with decoder
+        z = self.decoder(z)
 
-# MSE loss produces bad results
+        return z
+
+
 def reconstruction_loss_MSE(x, x_rec):
+    """
+    Compute the mean squared error (MSE) between original and reconstructed images.
+
+    :param x: Original images
+    :param x_rec: Reconstructed images
+    :return: MSE loss
+    """
     return torch.mean((x - x_rec) ** 2)
 
 
-# BCE loss produces best results
 def reconstruction_loss_BCE(x, x_rec):
+    """
+    Compute the binary cross entropy (BCE) between original and reconstructed images.
+
+    :param x: Original images
+    :param x_rec: Reconstructed images
+    :return: BCE loss
+    """
     # Add a channel dimension to x so it matches x_rec's shape
     x = x.unsqueeze(1)  # Shape will become [batch_size, 1, 28, 28]
     return torch.nn.functional.binary_cross_entropy(x_rec, x)
 
 
-def train_auto_encoder(batch_size=32, latent_dim=256, epochs=100, lr=1e-4, reconstruction_loss=reconstruction_loss_BCE,
-                       latent_initialization="normal", latent_reg_loss=1e-5, normal_latent_initialization_variance=0.1):
+def train_auto_encoder(batch_size=32, 
+                       latent_dim=256, 
+                       feature_map_size=256, 
+                       epochs=100, 
+                       lr=1e-4, 
+                       reconstruction_loss=reconstruction_loss_BCE,
+                       latent_initialization="normal", 
+                       latent_reg_loss_lambda=1e-5, 
+                       normal_latent_initialization_variance=0.1):
     """
-    latent_dim = 32/64/128/256
-    latent_reg_loss = 1e-5 or 1e-6 or 1e-4
-    normal_latent_initialization_variance = 0.01 or 0.1
+    Train the AutoDecoder on the Fashion MNIST dataset.
+
+    All of the function parameters are hyperparameters:
+
+    :param batch_size: Size of the mini-batches used for training
+    :param latent_dim: Dimensionality of the latent space
+    :param feature_map_size: Number of feature maps to first extract from the latent vector, before going throght CNN (e.g., 128/256/512)
+    :param epochs: Number of training epochs
+    :param lr: Learning rate for the optimizer
+    :param reconstruction_loss: Loss function for reconstruction (BCE or MSE)
+    :param latent_initialization: the distribution type to initialize latent vectors from ('normal', 'random', 'uniform')
+    :param latent_reg_loss_lambda: L2 regularization strength on latent vectors (if 0, there is no regularization)
+    :param normal_latent_initialization_variance: Variance used for normal initialization of latent vectors
     """
+    # load fashion-MNIST
     train_ds, train_dl, test_ds, test_dl = utils.create_dataloaders(data_path="dataset", batch_size=batch_size)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')    
 
-    model = AutoDecoder(latent_dim=latent_dim).to(device)
+    model = AutoDecoder(latent_dim=latent_dim, feature_map_size=feature_map_size).to(device)
 
-    # Initialize latent vectors for each sample in the training set
+    # Initialize a random latent vector for each sample in the training set
     if latent_initialization == "random":
-        latents = torch.randn(len(train_ds), latent_dim, requires_grad=True, device=device)  # Random initialization of latents
+        latents = torch.randn(len(train_ds), latent_dim, requires_grad=True, device=device)
     elif latent_initialization == "uniform":
         latents = torch.rand(len(train_ds), latent_dim, requires_grad=True, device=device)
     else:  # normal
         latents = torch.normal(0, normal_latent_initialization_variance, size=(len(train_ds), latent_dim), requires_grad=True, device=device)
 
-    # Optimizers
+    # define optimizers
     optimizer_model = torch.optim.Adam(model.parameters(), lr=lr)
-    optimizer_latents = torch.optim.Adam([latents], lr=lr)  # Optimizing the latents as well
+    # Since it ia an Auto Decoder, we optimize the latents as well
+    optimizer_latents = torch.optim.Adam([latents], lr=lr)  
+    # TODO: consider different lr for the model and latents
+    # TODO: consider a different optimizer (though Adam is considered good in general)
+
+    # Start total training time measurement
+    total_start_time = time.time()
 
     # Training loop
     for epoch in range(epochs):
-        model.train()
+        start_time = time.time()  # Start measuring time for this epoch
+        model.train()  # set model to training mode
         epoch_loss = 0
 
         for _, (indices, x) in enumerate(train_dl):
@@ -89,39 +134,45 @@ def train_auto_encoder(batch_size=32, latent_dim=256, epochs=100, lr=1e-4, recon
             indices = indices.to(device)
             x = x.to(device).float() / 255.0  # Normalize the input to [0, 1]
 
-            # Forward pass: get reconstructed images
+            # Forward pass: generate reconstructed images from latents
             latent_vectors = latents[indices]
             x_rec = model(latent_vectors)
 
             # Compute the reconstruction loss
-            if latent_reg_loss is not None:
-                reg_loss = latent_reg_loss * torch.norm(latents, p=2)  # Decrease the weight of regularization
-                loss = reconstruction_loss(x, x_rec) + reg_loss
-            else:
-                loss = reconstruction_loss(x, x_rec)
+            loss = reconstruction_loss(x, x_rec) + latent_reg_loss_lambda * torch.norm(latents, p=2)
 
             # Backpropagation
             optimizer_model.zero_grad()
             optimizer_latents.zero_grad()
             loss.backward()
 
-            # Update the model and latent vectors
+            # Update the model params and latent vectors
             optimizer_model.step()
             optimizer_latents.step()
 
             # Accumulate the loss for the epoch
             epoch_loss += loss.item()
 
+        # Calculate time taken for the epoch
+        end_time = time.time()
+        epoch_duration = end_time - start_time
+
         # Print the average loss per epoch
         avg_epoch_loss = epoch_loss / len(train_dl)
         print(f'Epoch [{epoch+1}/{epochs}], Loss: {avg_epoch_loss:.4f}')
+
+    # End total training time measurement
+    total_end_time = time.time()
+    total_training_time = total_end_time - total_start_time
+
+    # Print total training time
+    print(f'Total training time: {total_training_time:.2f} seconds')
 
     # Save model and latent vectors after training
     torch.save(model.state_dict(), 'auto_decoder.pth')
     torch.save(latents, 'latent_vectors.pth')
 
     # ---------------- visual evaluation ----------------
-
     sample_indices = torch.randint(0, len(train_ds), (5,))
     # Display original vs reconstructed images
     show_original_vs_reconstructed(model, latents, train_ds, sample_indices)
@@ -130,6 +181,7 @@ def train_auto_encoder(batch_size=32, latent_dim=256, epochs=100, lr=1e-4, recon
 def show_original_vs_reconstructed(model, latents, dataset, indices, num_samples=5):
     """
     Display original vs reconstructed images side by side.
+
     :param model: Trained AutoDecoder model
     :param latents: Latent vectors of the dataset
     :param dataset: The dataset from which to sample images
