546ccf46c88e9a70d22f2c9ac39591eba6232c30
Dana Gershon <dana.gershon@cs.technion.ac.il>
2024-09-20 10:30:33 +0300

    change env in py-sbatch to AD_Project

--- py-sbatch.sh
+++ py-sbatch.sh
@@ -36,7 +36,7 @@ MAIL_TYPE=ALL # Valid values are NONE, BEGIN, END, FAIL, REQUEUE, ALL
 # Conda parameters
 #
 CONDA_HOME=$HOME/miniconda3
-CONDA_ENV=cs236781-hw
+CONDA_ENV=AD_Project
 
 sbatch \
 	-N $NUM_NODES \
